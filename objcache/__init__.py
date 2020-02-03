bc2ed15467646fbf365d58e631ba6467b247c04a
Nick DeRobertis <whoopnip@gmail.com>
2020-02-03 08:06:22 -0500

    Add initial package files

--- objcache/__init__.py
+++ objcache/__init__.py
@@ -1,3 +1,5 @@
 """
-Simple Redis-like interface to storing arbirary Python objects. A high-level wrapper around ZoDB.
+A very simple API to storing Python objects built on ZODB. Use the .store method to store an object at a path,
+and .get to retrieve an object from a path.
 """
+from objcache.models.result_cache import ResultCache
